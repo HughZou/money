378cd3f26a8584db6d1ea5234cfaeecd57df6f40
HughZou <htcuuw@gmail.com>
2017-03-01 06:34:04 +0800

    modify

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Zouhua on 2017/3/1.
+ */
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+        });
+})
